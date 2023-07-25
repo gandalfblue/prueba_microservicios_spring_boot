2246bca0ab737175d243483b1e2ec067e458b91e
gandalfblue <casanova01_ahotmail.com>
2023-07-25 14:10:56 -0500

    Initial commit

---
+++ src/main/java/com/prueba_spring_boot/prueba_backend/PruebaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_spring_boot.prueba_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba_spring_boot/prueba_backend/PruebaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_spring_boot.prueba_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
