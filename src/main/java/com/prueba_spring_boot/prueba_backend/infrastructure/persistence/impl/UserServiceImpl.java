18b173a894873407405131f1301c7574829976b4
gandalfblue <casanova01_ahotmail.com>
2023-07-26 20:00:05 -0500

    feature Create service implementation of the clients and user

---
+++ src/main/java/com/prueba_spring_boot/prueba_backend/infrastructure/persistence/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.prueba_spring_boot.prueba_backend.infrastructure.persistence.impl;
+
+import com.prueba_spring_boot.prueba_backend.core.user.User;
+import com.prueba_spring_boot.prueba_backend.core.user.ports.UserRepositoryService;
+import com.prueba_spring_boot.prueba_backend.infrastructure.persistence.repositories.UserRepository;
+import com.prueba_spring_boot.prueba_backend.infrastructure.shared.exceptions.BadRequestException;
+import com.prueba_spring_boot.prueba_backend.infrastructure.shared.exceptions.NotFoundException;
+import lombok.AllArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserServiceImpl implements UserRepositoryService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public Mono<Boolean> getUser(User user) {
+		return userRepository
+				.findByDocumentNumber(user.getDocumentNumber())
+				.hasElement();
+	}
+
+	@Override
+	public Mono<Boolean> validateTypeDocument(User user) {
+		if (user.getType().equals("C") || user.getType().equals("P")){
+			return getUser(user);
+		}
+		return Mono.error(new BadRequestException("400 Type not supported"));
+	}
+}
